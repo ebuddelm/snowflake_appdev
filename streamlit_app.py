d0388ff01eeed7f1fbb20326cf2a2a92aaa75bde
ebuddelm <65282505+ebuddelm@users.noreply.github.com>
2023-08-23 14:34:50 +0200

    Update streamlit_app.py
    tabel maken

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# json to dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# dataframe display as a table
+streamlit.dataframe(fruityvice_normalized)
+
 
